66acb20ade630423c465162f5bd752b8025a1a14
Rodito Buan <jeff@buantel.com>
2026-03-11 09:11:30 -0400

    remove web
    create its own project

--- web/backend/src/db/pool.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config'
-import pg from 'pg'
-
-const connectionString = process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5433/jpaint'
-export const pool = new pg.Pool({ connectionString })
